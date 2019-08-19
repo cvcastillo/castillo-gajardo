460d69aacc988f2e4bc58398f94ea43148ff32b2
Gabu <fjgajardo@uc.cl>
2019-08-19 10:25:30 -0400

    Fotos integrante2

--- portafolio.js
+++ portafolio.js
@@ -3,43 +3,43 @@ var trabajos = [{
     alt: "Descripción de la imagen",
     title: "Título de primer trabajo",
     about: "Texto simulado de descripción de primer trabajo",
-    url: "www.google.com"
+    url: "https://www.google.com"
 }, {
     figure: "media/trabajo-02-app.jpg",
     alt: "Descripción de la imagen",
     title: "Título de segundo trabajo",
     about: "Texto simulado de descripción de segundo trabajo",
-    url: "www.google.com"
+    url: "https://www.google.com"
 }, {
     figure: "media/trabajo-03-homy.jpg",
     alt: "Descripción de la imagen",
     title: "Título de tercer trabajo",
     about: "Texto simulado de descripción de tercer trabajo",
-    url: "www.google.com"
+    url: "https://www.google.com"
 }, {
     figure: "media/trabajo-04-editorial.jpg",
     alt: "Descripción de la imagen",
     title: "Título de cuarto trabajo",
     about: "Texto simulado de descripción de cuarto trabajo",
-    url: "www.google.com"
+    url: "https://www.google.com"
 }, {
     figure: "media/trabajo-05-artesania.jpg",
     alt: "Descripción de la imagen",
     title: "Título de quinto trabajo",
     about: "Texto simulado de descripción de quinto trabajo",
-    url: "www.google.com"
+    url: "https://www.google.com"
 }, {
     figure: "media/trabajo-06-textil.jpg",
     alt: "Descripción de la imagen",
     title: "Título de sexto trabajo",
     about: "Texto simulado de descripción de sexto trabajo",
-    url: "www.google.com"
+    url: "https://www.google.com"
 }, {
     figure: "media/trabajo-07-materiales.jpg",
     alt: "Descripción de la 3e3e3imagen",
     title: "Título de séptimo trabajo",
     about: "Texto simulado de descripción de séptimo trabajo",
-    url: "www.google.com"
+    url: "https://www.google.com"
 }];
 // LUEGO CREAMOS PARTES DEL SITIO CON CADA DATO CONTENIDO EN LA VARIABLE
 $.each(trabajos, function(i, val) {
